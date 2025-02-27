699d5fb88791e9bc89ab45165ecff665e7c22b17
Alex Ioannidis <a.ioannidis@cern.ch>
2025-02-27 08:22:30 +0100

    global: update deps and remove views

--- tests/test_invenio_checks.py
+++ tests/test_invenio_checks.py
@@ -30,10 +30,3 @@ def test_init():
     assert "invenio-checks" not in app.extensions
     ext.init_app(app)
     assert "invenio-checks" in app.extensions
-
-
-def test_view(base_client):
-    """Test view."""
-    res = base_client.get("/")
-    assert res.status_code == 200
-    assert "Welcome to Invenio-Checks" in str(res.data)
