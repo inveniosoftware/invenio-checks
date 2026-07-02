f03da2fc6202ebeb045f003d09f8df0bfd305761
Sam Arbid <samaa2@kth.se>
2026-07-02 13:43:47 +0200

    📦 release: v10.1.0

--- invenio_checks/__init__.py
+++ invenio_checks/__init__.py
@@ -1,11 +1,12 @@
 # SPDX-FileCopyrightText: 2025-2026 CERN.
 # SPDX-FileCopyrightText: 2026 Graz University of Technology.
+# SPDX-FileCopyrightText: 2026 KTH Royal Institute of Technology.
 # SPDX-License-Identifier: MIT
 
 """Invenio module to automated curation checks on records."""
 
 from .ext import InvenioChecks
 
-__version__ = "10.0.0"
+__version__ = "10.1.0"
 
 __all__ = ("__version__", "InvenioChecks")
